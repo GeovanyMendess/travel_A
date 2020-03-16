2d30bd5432394b7d727cc374deacee3c7ad576fb
Geovany Mendes <geovany.c.mendes@gmail.com>
2020-03-16 18:10:28 -0300

    Last comments

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-// imported libraries
+// Imported libraries
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes')
 
-// initializing aplication
+// Initializing aplication
 const app = express();
 
 // connectiing to database
@@ -14,13 +14,13 @@ mongoose.connect('mongodb+srv://Geovany:7wf3e23QYyEP7st@cluster0-siiem.gcp.mongo
 });
 
 app.get('/', (request, response)=> {
-  return response.json({message : 'Olá mundo'});
 });
 
 // Midlleware managers
-// This allow the aplication understand the the JSON format
 app.use(cors());
+// This allow the aplication understand the the JSON format
 app.use(express.json());
+// Controlling routes
 app.use(routes);
 
 // Chosing a door to the aplication
